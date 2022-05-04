4a6394dc7e4e33f8e96768314547f2b4280aebf4
adamciatti1 <adam_ciatti1@baylor.edu>
2022-05-04 08:26:57 -0500

    hw 5-4

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,14 @@ from django.urls import path, inlcude
 
 app_name = 'users'
 
+from . import views 
+
+
+app_name = 'users' 
+
 urlpatterns = [
+    # Include default auth urls
     path('',inlcude('django.contrib.auth.urls')),
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
